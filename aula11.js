c8d74945ca980c32e632c18b80eb18b809a8d156
João Pedro <joaocarvalhop1@outlook.com>
2023-04-08 11:16:58 -0300

    aula11 and aula12

---
+++ aula11.js
@@ -0,0 +1,9 @@
+let firstname  = prompt("firstname")
+let lastname   = prompt("lastname")
+let studyfield = prompt("studyfield")
+let yearofbirth   = prompt("year of birth")
+
+age = (yearofbirth - 2023) * -1
+
+alert("NOME COMPLETO: " + firstname + " " + lastname)
+alert("ESTUDA: " + studyfield + " POSSUI: " + age + " ANOS DE IDADE")
---
+++ aula12.js
@@ -0,0 +1,9 @@
+num1 = prompt("manda o num1")
+num2 = prompt("manda o num2")
+
+soma          = parseInt(num1 + num2)
+subtracao     = num1 - num2
+multiplicacao = num1 * num2
+divisao       = num1 / num2
+
+alert("soma " + soma + " sub " + subtracao + " multi " + multiplicacao + " divis " + divisao)
